fd1d915fc14d0e068904bc6e1d03fc18cf9d8e02
Jaison <jaisonschmidt@gmail.com>
2024-06-10 20:31:00 -0300

    Finaliza componente ProfileInfo

--- my-website/components/ProfileInfo.vue
+++ my-website/components/ProfileInfo.vue
@@ -4,19 +4,19 @@
       <figure class="p-6">
         <img
           class="w-32 h-32 rounded-full mx-auto"
-          src="./assets/profile.png"
+          :src="photo"
           alt="profile picture"
           width="400"
           height="400"
         />
         <div class="pt-6 text-center space-y-4">
           <figcaption class="font-medium">
-            <div class="text-cyan-900 text-xl">Carlos Pinto</div>
-            <div class="text-gray-500 font-light">@cpintoval</div>
+            <div class="text-cyan-900 text-xl">{{ title }}</div>
+            <div class="text-gray-500 font-light">{{ subtitle }}</div>
           </figcaption>
           <blockquote>
             <p class="text-lg font-medium">
-              Technology | Product | Data & Analytics
+              {{ heading }}
             </p>
           </blockquote>
         </div>
