75a8b845e2af99f784383a43913ff63e67836d1b
Jaison <jaisonschmidt@gmail.com>
2024-06-10 20:27:28 -0300

    Cria props no componente ProfileInfo

---
+++ my-website/components/ProfileInfo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="max-w-7xl mx-auto">
+    <div>
+      <figure class="p-6">
+        <img
+          class="w-32 h-32 rounded-full mx-auto"
+          src="./assets/profile.png"
+          alt="profile picture"
+          width="400"
+          height="400"
+        />
+        <div class="pt-6 text-center space-y-4">
+          <figcaption class="font-medium">
+            <div class="text-cyan-900 text-xl">Carlos Pinto</div>
+            <div class="text-gray-500 font-light">@cpintoval</div>
+          </figcaption>
+          <blockquote>
+            <p class="text-lg font-medium">
+              Technology | Product | Data & Analytics
+            </p>
+          </blockquote>
+        </div>
+      </figure>
+    </div>
+  </div>
+</template>
+
+<script setup>
+    const { photo = null, title = '', subtitle = '', heading = '' } = defineProps(['photo', 'title', 'subtitle', 'heading']);
+</script>
